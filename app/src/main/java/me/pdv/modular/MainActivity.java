3ae422b34bb602672d86429b8d2da6e58ac98dc5
pdv <philipjdelvecchio@gmail.com>
2017-01-01 17:31:28 -0500

    Java 8, RxJava

--- app/src/main/java/me/pdv/modular/MainActivity.java
+++ app/src/main/java/me/pdv/modular/MainActivity.java
@@ -3,11 +3,17 @@ package me.pdv.modular;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rx.Observable;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable.just(1, 2, 3)
+                .filter(x -> x > 2)
+                .subscribe(System.out::println);
     }
 }
