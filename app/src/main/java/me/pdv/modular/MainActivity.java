9dafda41d9ea68b5d456085cd20379a0cd5a1266
pdv <philipjdelvecchio@gmail.com>
2017-02-01 15:59:41 -0500

    Add Counter

--- app/src/main/java/me/pdv/modular/MainActivity.java
+++ app/src/main/java/me/pdv/modular/MainActivity.java
@@ -2,12 +2,12 @@ package me.pdv.modular;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-import com.jakewharton.rxbinding.widget.RxTextView;
+import com.jakewharton.rxbinding.view.RxView;
 
 import java.util.List;
+import java.util.Locale;
 
 import rx.Observable;
 
@@ -18,15 +18,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Repository repository = new Repository();
-        TeamsAdapter adapter = new TeamsAdapter();
+        TextView result = (TextView) findViewById(R.id.tv_result);
 
-        TextView textView = new TextView(this);
+        Observable<Void> upClicks = RxView.clicks(findViewById(R.id.btn_up));
+        Observable<Void> downClicks = RxView.clicks(findViewById(R.id.btn_down));
+        Counter.count(upClicks, downClicks)
+                .compose(Counter::format)
+                .subscribe(result::setText);
+    }
 
-        Search.Repository searchRepo = repository::getTeamsForSearch;
-        Search.QueryView queryView = () -> RxTextView.textChanges(textView);
-        Search.ResultsView resultsView = adapter::setTeams;
-        Search.displaySearchResults(searchRepo, queryView, resultsView, Timber::e);
+
+    static class MathUtil {
+        public static int sum(Integer... ints) {
+            int ret = 0;
+            for (int i : ints) {
+                ret += i;
+            }
+            return ret;
+        }
+    }
+
+    static class Counter {
+
+        public static Observable<Integer> count(Observable<Void> upClicks, Observable<Void> downClicks) {
+            return Observable.merge(
+                    upClicks.map(__ -> 1),
+                    downClicks.map(__ -> -1))
+                    .startWith(0)
+                    .scan(0, MathUtil::sum);
+        }
+
+        public static Observable<String> format(Observable<Integer> count) {
+            return count.map(countVal -> String.format(Locale.getDefault(), "%d", countVal));
+        }
+
     }
 
     static class Timber {
