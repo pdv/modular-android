129edef3c1cc5fdfa3ad2770e1927dfbed1f4f21
Phil Del Vecchio <philipjdelvecchio@gmail.com>
2017-02-18 22:59:58 -0500

    Fix terrible merge

--- app/src/main/java/me/pdv/modular/Counter.java
+++ app/src/main/java/me/pdv/modular/Counter.java
@@ -1,38 +1,5 @@
 package me.pdv.modular;
 
-<<<<<<< HEAD
-import android.util.Log;
-
-import rx.Observable;
-import rx.Subscription;
-
-/**
- * Created by pdv on 1/31/2017.
- */
-
-public final class Counter {
-
-    public static int sum(int... ints) {
-        int sum = 0;
-        for (int i : ints) {
-            sum += i;
-        }
-        return sum;
-    }
-
-    public static Subscription count(Observable<Void> upClicks,
-                                     Observable<Void> downClicks,
-                                     M.Sink<String> count) {
-        return Observable.merge(
-                upClicks.map(__ -> 1),
-                downClicks.map(__ -> -1))
-                .scan(0, Counter::sum)
-                .map(String::valueOf)
-                .subscribe(count::put);
-    }
-
-}
-=======
 import rx.Observable;
 
 /**
@@ -56,6 +23,3 @@ final class Counter {
 
 }
 
-
-
->>>>>>> 3ff1ee7b0d1c79618d4396dfd3eecd7ac9d7d0c6
--- app/src/main/java/me/pdv/modular/MainActivity.java
+++ app/src/main/java/me/pdv/modular/MainActivity.java
@@ -2,21 +2,12 @@ package me.pdv.modular;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.jakewharton.rxbinding.view.RxView;
-=======
-import android.view.View;
-import android.widget.TextView;
 
-import com.jakewharton.rxbinding.view.RxView;
-import com.jakewharton.rxbinding.widget.RxTextView;
->>>>>>> 3ff1ee7b0d1c79618d4396dfd3eecd7ac9d7d0c6
-
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
@@ -28,27 +19,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
         TextView countView = (TextView) findViewById(R.id.tv_count);
         Button upButton = (Button) findViewById(R.id.btn_up);
         Button downButton = (Button) findViewById(R.id.btn_down);
-        Counter.count(RxView.clicks(upButton), RxView.clicks(downButton), countView::setText);
-=======
-        TextView result = (TextView) findViewById(R.id.tv_result);
+        // Counter.count(RxView.clicks(upButton), RxView.clicks(downButton), countView::setText);
+
         View view = new View(this);
-
-        view.setOnClickListener(v -> foo());
-
-        List<Integer> list = Arrays.asList(1, 2, 3);
-        list.map(x -> x + 1)
-            .forEach(System.out::println);
-
-        Observable<Integer> stream = Observable.just(1, 2, 3);
-        stream.map(x -> x + 1)
-              .subscribe(System.out::println);
-
-        View upButton = new View(this);
-        View downButton = new View(this);
 
         Observable.merge(
                 RxView.clicks(upButton).map(__ -> 1),
@@ -62,9 +38,6 @@ public class MainActivity extends AppCompatActivity {
                 .map(String::valueOf)
                 .subscribe(countView::setText);
 
-        view.setOnClickListener(__ -> incrementCount());
-
-
         Observable<Long> upTicks = Observable.interval(5, TimeUnit.SECONDS);
         Observable<?> downTicks = Observable.timer(10, TimeUnit.MINUTES);
         Counter.count(upTicks, downTicks)
@@ -76,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void playSound(Sound sound) {
 
->>>>>>> 3ff1ee7b0d1c79618d4396dfd3eecd7ac9d7d0c6
     }
 
     private static class Sound {
@@ -87,22 +59,6 @@ public class MainActivity extends AppCompatActivity {
         static Sound textToSpeech(String text) {
             return null;
         }
-    }
-
-    TextView countView = new TextView(this);
-
-
-    private void incrementCount() {
-        count += 1;
-        updateCountView();
-    }
-
-    private void updateCountView() {
-        countView.setText(String.valueOf(count));
-    }
-
-    private static void foo() {
-
     }
 
 }
